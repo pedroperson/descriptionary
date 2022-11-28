5fcc8bf3a37c467dd1179919805e8c8d8aaf05c8
BitmanDuran <eduransa@umd.edu>
2022-11-27 21:09:04 -0500

    Header styling

--- client/src/lib/components/Header.svelte
+++ client/src/lib/components/Header.svelte
@@ -7,9 +7,23 @@
 </header>
 
 <style>
+
 	header {
+		width: 1280px;
+		height: 50px;
+
+		font-family: 'Helvetica';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 20px;
+		line-height: 14px;
+
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
+
+		color: #000000;
+		background: #D9D9D9;
 	}
 
 	h1 {
